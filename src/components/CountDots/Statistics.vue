41e36d743e98418ae8286dbce1cac33dc8e6ca32
Dmitry Weiner <dmitry.weiner@gmail.com>
2019-06-27 13:33:01 +0700

    Commit forgotten

---
+++ src/components/CountDots/Statistics.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="statistics">
+    <div class="correct">
+      <template v-for="i in correct">{{'\u{1F36C}'}}</template>
+    </div>
+    <div class="wrong">
+      <template v-for="i in wrong">{{'\u{1F41B}'}}</template>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class CountDots extends Vue {
+    @Prop() public correct!: number;
+    @Prop() public wrong!: number;
+  }
+</script>
+
+<style scoped>
+  .statistics {
+    width: 100%;
+    margin-top: 40px;
+    font-size: 40px;
+  }
+
+  .statistics .correct, .statistics .wrong {
+    width: 50%;
+    margin: 0 auto;
+  }
+</style>
