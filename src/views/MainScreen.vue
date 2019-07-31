eb64ba19fa475b1d6cd1407660d31f88750b4173
Dmitry Weiner <dmitry.weiner@gmail.com>
2019-07-31 12:42:36 +0700

    Add link

--- src/views/MainScreen.vue
+++ src/views/MainScreen.vue
@@ -4,6 +4,7 @@
     <h3><router-link to="/countItems">Посчитай картинки</router-link></h3>
     <h3><router-link to="/solveCalculus">Реши пример</router-link></h3>
     <h3><router-link to="/guessFirstLetter">Угадай букву</router-link></h3>
+    <h3><router-link to="/englishLearning">Учим английский</router-link></h3>
   </div>
 </template>
 
